805f1734ba09499edf6a0501a91b6cba38b88282
sr76 <santiago.rigamonti@physik.hu-berlin.de>
2012-08-21 18:43:56 +0200

    working on optimize.py script. Redoing some lost stuf.

--- optimize.py
+++ optimize.py
@@ -4,9 +4,15 @@ import sys
 import setinput
 from lxml import etree
 
-def optimize(runpath,resultspath,inputtemplatepath,varxpatharr,varvaluearr,varscalearr,ftolerance=1.e-4,xtolerance=1.e-4,itmax=500,runcommandstring):
+def optimize(runpath,resultspath,inputtemplatepath,vararr,ftolerance=1.e-4,xtolerance=1.e-4,itmax=500,runcommandstring):
     optimizelog = open(resultspath+"/optimize.log","w+")
     optimizelog.write("runpath  "+runspath+"\n")
     optimizelog.write("resultspath  "+resultspath+"\n")
     optimizelog.write("inputtemplatepath  "+inputtemplatepath+"\n")
     
+
+
+def energy(var, data):
+    runpath = data[0]
+    inputtemplatepath = data[1]
+    runcommand = data[2]
