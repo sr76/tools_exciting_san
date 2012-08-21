4ab699332c9329d55e14205d21bb8db8fa79177f
sr76 <santiago.rigamonti@physik.hu-berlin.de>
2012-08-21 15:54:58 +0200

    moved setinput.py to setinputxml.py because this naming is more compatible with the newly created queryinputxml.py

--- queryinputxml.py
+++ queryinputxml.py
@@ -6,4 +6,17 @@ import sys
 
 def getByXpath(xpath,fname):
     tree = etree.parse(fname)
-    return tree.xpath(xpath)
+    if "@" in xpath:
+        return tree.xpath(xpath)[0]
+    else:
+        return tree.xpath(xpath)[0].text
+
+
+
+xpath = "/input/structure/crystal/basevect[3]"
+xpath = "/input/structure/crystal/@scale"
+xpath = "/input/structure/crystal/basevect[3]"
+fname = "/home1/srigamonti/projects/cobalt_bulk/runs/1345479936227/input.xml"
+
+
+print getByXpath(xpath,fname)
