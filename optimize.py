5b4834d6dc7e9e5bf6d51af50ddad0993a49ca7d
sr76 <santiago.rigamonti@physik.hu-berlin.de>
2012-08-21 19:20:11 +0200

    Continue working on optimize.py script. Partially finished energy(var, data) function in the script

--- optimize.py
+++ optimize.py
@@ -1,18 +1,48 @@
 import time
 import os
 import sys
-import setinput
+import setinputxml
+import queryinfoxml
 from lxml import etree
+import tstamp_folder
 
 def optimize(runpath,resultspath,inputtemplatepath,vararr,ftolerance=1.e-4,xtolerance=1.e-4,itmax=500,runcommandstring):
     optimizelog = open(resultspath+"/optimize.log","w+")
     optimizelog.write("runpath  "+runspath+"\n")
     optimizelog.write("resultspath  "+resultspath+"\n")
     optimizelog.write("inputtemplatepath  "+inputtemplatepath+"\n")
-    
+        
 
 
 def energy(var, data):
     runpath = data[0]
     inputtemplatepath = data[1]
     runcommand = data[2]
+    xpath = data[3]
+    
+    os.chdir(runpath)
+    
+    inputtree = etree.parse(inputtemplatepath)
+
+    nvar = len(var)
+
+    for i in range(nvar):
+        setinputxml.setByXpath(inputtree,xpath[i],str(var[i]))
+
+    inputtree.write(runpath+"/input.xml",pretty_print="true")
+
+    os.chdir(runpath)
+    os.system("rm -f info.xml")
+    
+    os.system(runcommand)
+    
+    while not os.path.exists("info.xml"):
+        time.sleep(1)
+
+    return -queryinfoxml.getLastTotalEnergy()
+
+
+runpath = tstamp_folder.tstamp_folder("/home1/srigamonti/projects/cobalt_bulk/runs")
+resultspath = "/home1/srigamonti/projects/cobalt_bulk/results/optimizeunitcell"
+
+
--- queryinfoxml.py
+++ queryinfoxml.py
@@ -4,6 +4,6 @@ import os
 import sys
 
 
-def getLastTotalEnergy(fname):
-    tree = etree.parse(fname)
+def getLastTotalEnergy():
+    tree = etree.parse("info.xml")
     return tree.xpath('/info/groundstate/scl/iter[last()]/energies/@totalEnergy')
