a3642e082d05f7616796037887de831b557600f4
sr76 <santiago.rigamonti@physik.hu-berlin.de>
2012-08-22 13:51:07 +0200

    first working version of setByXpath in setinputxml.py where xpath of the type /input/structure/crystal/basevect[i][j], with i and j integers, can be passed.

--- setinputxml.py
+++ setinputxml.py
@@ -1,6 +1,45 @@
 from lxml import etree
 
 
+"""
+# Example of use:
+
+from lxml import etree
+
+inputtree = etree.parse("input.xml")
+xpath = "/input/groundstate/@nempty"
+value = 5
+
+setinputxml.setByXpath(inputtree,xpath,value)
+
+inputtree.write("input.xml",pretty_print="true")
+
+# Another example of use:
+
+from lxml import etree
+
+inputtree = etree.parse("input.xml")
+xpath = "/input/keywords" # or equivalently "/input/keywords[1]"
+value = "Keywords for the run"
+
+setinputxml.setByXpath(inputtree,xpath,value)
+
+inputtree.write("input.xml",pretty_print="true")
+
+# Example of xpath to change the third basevect of the crystal structure definition:
+
+from lxml import etree
+
+inputtree = etree.parse("input.xml")
+xpath = "/input/structure/crystal/basevect[3]" # or equivalently "/input/keywords[1]"
+value = "Keywords for the run"
+
+setinputxml.setByXpath(inputtree,xpath,value)
+
+inputtree.write("input.xml",pretty_print="true")
+
+"""
+
 
 def setByXpath(inputtree,xpath,value):
     if "@" in xpath:
@@ -9,5 +48,30 @@ def setByXpath(inputtree,xpath,value):
         inputtree.xpath(element)[0].attrib[attrib]=value
 
     else:
-        inputtree.xpath(xpath)[0].text = value
+        if "][" in xpath:
+            # In this case, the xpath is something like
+            # /input/structure/crystal/basevect[3][3]
+            # where we want to address the third component of the third basevect vector.
+            basexpath = xpath.split("][")[-2]+"]"
+            oldtext = inputtree.xpath(basexpath)[0].text
+            stext = oldtext.split()
+            component = int(xpath.split("][")[1][:-1])
+            stext[component-1] = str(value)
+            newtext = "  ".join(stext)
+            inputtree.xpath(basexpath)[0].text = newtext
+        else:
+            inputtree.xpath(xpath)[0].text = value
 
+
+"""
+inputtree = etree.parse("/home1/srigamonti/projects/cobalt_bulk/runs/1345479936227/input.xml")
+
+xpath = "/input/structure/crystal/basevect[3][3]"
+
+value = "Keywords for the run"
+
+setByXpath(inputtree,xpath,value)
+
+#inputtree.write("input.xml",pretty_print="true")
+print etree.tostring(inputtree)
+"""
