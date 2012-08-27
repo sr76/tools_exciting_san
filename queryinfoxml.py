3d80f065c06251cefabdbc919906c5c95a0d1be2
sr76 <santiago.rigamonti@physik.hu-berlin.de>
2012-08-27 17:30:02 +0200

    added various get functions to queryinfoxml.py

--- queryinfoxml.py
+++ queryinfoxml.py
@@ -8,6 +8,14 @@ def getLastTotalEnergy():
     tree = etree.parse("info.xml")
     return float(tree.xpath('/info/groundstate/scl/iter[last()]/energies/@totalEnergy')[0])
 
+def getNumIter():
+    tree = etree.parse("info.xml")
+    return int(tree.xpath('/info/groundstate/scl/iter[last()]/@iteration')[0])
+
+def getTotalMagneticMoment():
+    tree = etree.parse("info.xml")
+    return float(tree.xpath('/info/groundstate/scl/iter[last()]/moments/momtot/@x')[0])
+
 
 """
 # Just for testing purposes
